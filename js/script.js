4dc98f81e028d1d84fca8a00efceed6448ae8692
SrirachaSiriah <Steelesiriah@gmail.com>
2020-09-15 15:20:08 +1200

    made responsive and added burger menu

--- js/script.js
+++ js/script.js
@@ -1,50 +1,70 @@
 $(document).ready(function(){
 
+//Click to go to each tab
+
   $('.entrees').show();
   $('.mains').hide();
   $('.drinks').hide();
   $('.specials').hide();
   $('.cart').hide();
 
-    $('#entrees').click(function(){
-      $('.entrees').show();
-      $('.mains').hide();
-      $('.drinks').hide();
-      $('.specials').hide();
-      $('.cart').hide();
-    });
+  $('#entrees').click(function(){
+    $('.entrees').show();
+    $('.mains').hide();
+    $('.drinks').hide();
+    $('.specials').hide();
+    $('.cart').hide();
+  });
 
-    $('#mains').click(function(){
-      $('.entrees').hide();
-      $('.mains').show();
-      $('.drinks').hide();
-      $('.specials').hide();
-      $('.cart').hide();
-    });
+  $('#mains').click(function(){
+    $('.entrees').hide();
+    $('.mains').show();
+    $('.drinks').hide();
+    $('.specials').hide();
+    $('.cart').hide();
+  });
 
-    $('#drinks').click(function(){
-      $('.entrees').hide();
-      $('.mains').hide();
-      $('.drinks').show();
-      $('.specials').hide();
-      $('.cart').hide();
-    });
+  $('#drinks').click(function(){
+    $('.entrees').hide();
+    $('.mains').hide();
+    $('.drinks').show();
+    $('.specials').hide();
+    $('.cart').hide();
+  });
 
-    $('#specials').click(function(){
-      $('.entrees').hide();
-      $('.mains').hide();
-      $('.drinks').hide();
-      $('.specials').show();
-      $('.cart').hide();
-    });
+  $('#specials').click(function(){
+    $('.entrees').hide();
+    $('.mains').hide();
+    $('.drinks').hide();
+    $('.specials').show();
+    $('.cart').hide();
+  });
 
-    $('#cart').click(function(){
-      $('.entrees').hide();
-      $('.mains').hide();
-      $('.drinks').hide();
-      $('.specials').hide();
-      $('.cart').show();
+  $('#cart').click(function(){
+    $('.entrees').hide();
+    $('.mains').hide();
+    $('.drinks').hide();
+    $('.specials').hide();
+    $('.cart').show();
+  });
 
-    });
+  $('#order').click(function(){
+    $('.entrees').hide();
+    $('.mains').hide();
+    $('.drinks').hide();
+    $('.specials').hide();
+    $('.cart').show();
+  });
+
+//Mobile Navbar Code
+
+  $('.burger_menu').click(function(){
+      $('.top_nav').toggleClass('active');
+
+
+
+  });
+
+
 
 });
