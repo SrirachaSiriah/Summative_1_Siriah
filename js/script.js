8a14ededec14a8cba6048a553d9f9d317680b267
SrirachaSiriah <Steelesiriah@gmail.com>
2020-09-17 15:14:26 +1200

    added the bulk of the Mains HTML

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ $(document).ready(function(){
   $('.drinks').hide();
   $('.specials').hide();
   $('.cart').hide();
+  $('.add_item_display').hide();
 
   $('#entrees').click(function(){
     $('.entrees').show();
@@ -14,6 +15,7 @@ $(document).ready(function(){
     $('.drinks').hide();
     $('.specials').hide();
     $('.cart').hide();
+    $('.add_item_display').hide();
   });
 
   $('#mains').click(function(){
@@ -22,6 +24,7 @@ $(document).ready(function(){
     $('.drinks').hide();
     $('.specials').hide();
     $('.cart').hide();
+    $('.add_item_display').hide();
   });
 
   $('#drinks').click(function(){
@@ -30,6 +33,7 @@ $(document).ready(function(){
     $('.drinks').show();
     $('.specials').hide();
     $('.cart').hide();
+    $('.add_item_display').hide();
   });
 
   $('#specials').click(function(){
@@ -38,6 +42,7 @@ $(document).ready(function(){
     $('.drinks').hide();
     $('.specials').show();
     $('.cart').hide();
+    $('.add_item_display').hide();
   });
 
   $('#cart').click(function(){
@@ -46,6 +51,7 @@ $(document).ready(function(){
     $('.drinks').hide();
     $('.specials').hide();
     $('.cart').show();
+    $('.add_item_display').hide();
   });
 
   $('#order').click(function(){
@@ -54,17 +60,76 @@ $(document).ready(function(){
     $('.drinks').hide();
     $('.specials').hide();
     $('.cart').show();
+    $('.add_item_display').hide();
   });
+
+//Add Item Display
+
+  $('.food_image').click(function(){
+    $('.add_item_display').show();
+    $('.entrees').hide();
+    $('.mains').hide();
+    $('.drinks').hide();
+    $('.specials').hide();
+    $('.cart').hide();
+  });
+
+  $('.close_white_box').click(function(){
+    $('.add_item_display').hide();
+    $('.entrees').show();
+    $('.mains').hide();
+    $('.drinks').hide();
+    $('.specials').hide();
+    $('.cart').hide();
+  });
+
 
 //Mobile Navbar Code
 
   $('.burger_menu').click(function(){
       $('.top_nav').toggleClass('active');
+  });
+//
+
+  $('.order_add').click(function(){
+    $('.add_item_display').hide();
+    $('.entrees').hide();
+    $('.mains').hide();
+    $('.drinks').hide();
+    $('.specials').hide();
+    $('.cart').show();
+  });
+//
+
+  $('.food_image').on('click', function(){
+     console.log(this.id);
+    var itemName = this.id //stogin the value to a variable
+    $('#cartFoodName').append ('<br>' + itemName);
 
 
+    // console.log($('.menu-button').index(this));
+    var myIndex = $('.food_image').index(this);
+    console.log(myIndex);
+
+    // var itemPrice = document.getElementsByClassName('item-price');
+    // console.log(itemPrice);
+    //
+    // for (var i=0; i<itemPrice.length; i++){
+    //
+    //   if (myIndex == i){
+    //     console.log(itemPrice[i].innerHTML);
+    //     var price = itemPrice[i].innerHTML;
+    //     $('#price').append ('<br>' + price);
+    //   }
+
+    // }
 
   });
 
+  $('.order_add').on('click', function(){
+    var myIndex = $('.food_image').index(this);
+    $('#items').append ('<br>' + itemName);
 
+  });
 
 });
